09f5c27cd2a376fa8f8ca04cffe25f5bce3efbf3
danielmoessner <github@danielmoessner.de>
2026-04-18 12:50:43 +0200

    feat: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,13 @@ export default function RootLayout({
   return (
     <html lang="de">
       <head>
+        <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="BZV" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&display=swap" rel="stylesheet" />
